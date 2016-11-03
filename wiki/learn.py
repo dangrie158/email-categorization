6fd2ffd5f342e6209166d3c192a134ca98c25360
Daniel Grießhaber <dangrie158@gmail.com>
2016-11-03 13:53:13 +0100

    added phrase detection to learning strategy and removed init_sims optimization to allow further model learning

--- wiki/learn.py
+++ wiki/learn.py
@@ -4,30 +4,34 @@ import logging
 import os.path
 import sys
 import multiprocessing
- 
+
 from gensim.corpora import  WikiCorpus
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
- 
- 
+from gensim.models import Phrases
+
+
 if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
     logger = logging.getLogger(program)
- 
+
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
     logging.root.setLevel(level=logging.INFO)
     logger.info("running %s" % ' '.join(sys.argv))
- 
+
     # check and process input arguments
- 
+
     if len(sys.argv) < 3:
         print globals()['__doc__'] % locals()
         sys.exit(1)
     inp, outp = sys.argv[1:3]
- 
-    model = Word2Vec(LineSentence(inp), size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
- 
+
+    # create a phrase detector to learn phrases
+    bigram_transformer = Phrases(LineSentence(inp))
+    model = Word2Vec(bigram_transformer, size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
+
     # trim unneeded model memory = use (much) less RAM
-    model.init_sims(replace=True)
- 
+    # doing this results in a model that is not further trainable so don't do it here
+    # model.init_sims(replace=True)
+
     model.save(outp)
