8b05cfe1c4e4408677c5f9208f1c7fd1b769fd30
Daniel Grießhaber <dangrie158@gmail.com>
2016-12-12 21:27:52 +0100

    changed generateSet to use a fraction of the whole corpus instead of a fixed number of samples

--- mail/generateSets.py
+++ mail/generateSets.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-Usage %s input_file out_training_file out_validation_file validation_samples [seed]
+Usage %s input_file out_training_file out_validation_file validation_sample_ratio [seed]
 
 Split a TextCorpus into a Training and validation set
 
 This will output 2 textfiles; one out_validation_file
-that contains random validation_samples lines from the input
+that contains random lines from the input (validation_sample_ratio \% of the lines are used)
 file and a file out_training_file that contains all other files
 """
 
@@ -26,9 +26,8 @@ if __name__ == '__main__':
         print __doc__ % sys.argv[0].split("/")[-1]
         sys.exit(1)
 
-    in_file, out_training, out_validation, validation_samples = sys.argv[1:5]
+    in_file, out_training, out_validation, validation_sample_ratio = sys.argv[1:5]
 
-    print("generating training sets from {} with {} lines for validation".format(in_file, validation_samples))
 
     if len(sys.argv) >= 6:
         print("seeding prng with {}".format(sys.argv[5]))
@@ -38,11 +37,15 @@ if __name__ == '__main__':
     print('counting lines')
     lines = get_lines(in_file)
 
+    validation_samples = int((float(len(lines)) / 100) * float(validation_sample_ratio))
+    print("generating training sets from {} with {} lines for validation".format(in_file, validation_samples))
+
+
     #shuffle the lines
     random.shuffle(lines)
 
     #create the sets
-    validation_lines = lines[:int(validation_samples)]
+    validation_lines = lines[:validation_samples]
 
     print("extracted {} random lines for validation from a total of {} lines".format(len(validation_lines), len(lines)))
 
